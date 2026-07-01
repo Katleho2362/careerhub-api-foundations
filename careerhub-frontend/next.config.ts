ff4cda901522360f5d308811fb5db6b79cc216ca
KE Tlale <katlehoedward1@gmail.com>
2026-07-01 20:09:59 +0200

    feat(perf): add remotePatterns for dicebear logo domain

--- careerhub-frontend/next.config.ts
+++ careerhub-frontend/next.config.ts
@@ -1,7 +1,40 @@
+// import type { NextConfig } from "next";
+// import withBundleAnalyzer from "@next/bundle-analyzer";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     dangerouslyAllowSVG: true,
+//     contentDispositionType: "attachment",
+//     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "api.dicebear.com",
+//         pathname: "/7.x/initials/svg/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default withBundleAnalyzer({
+//   enabled: process.env.ANALYZE === "true",
+// })(nextConfig);
+
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+        pathname: "/7.x/initials/png/**",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
